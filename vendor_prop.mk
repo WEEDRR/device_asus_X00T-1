KomodOSRom-Devices/device_asus_X00T

X00T: Now Official

vendor_prop.mk
@@ -262,3 +262,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.dha_th_rate=2.3 \
     ro.config.sdha_apps_bg_max=64 \
     ro.config.sdha_apps_bg_min=8
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    org.komodo.maintainer=zul

WEEDRR/device_asus_X00T-1

vendor_prop.mk
@@ -231,3 +231,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # System prop for UBWC
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.video.disable.ubwc=1
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    org.komodo.maintainer=zulfkennedy